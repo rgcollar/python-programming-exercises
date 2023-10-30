ce93eb492f39d24764560d1d7969e0aa4ff66f2c
Gustavo Collar <rodrigogustavocollar@gmail.com>
2023-10-30 13:47:57 -0300

    :sparkles: Added solution to exercise 1)

---
+++ power-two/solution1.py
@@ -0,0 +1,16 @@
+def is_power_of_2(n):
+    if n == 1:
+        return True
+    elif n % 2 == 0:
+        return is_power_of_2(n // 2)
+    else:
+        return False
+
+
+l_ints = list(range(1, int(input('Please enter a number: '))))
+
+for number in l_ints:
+    if is_power_of_2(number):
+        print(f"{number} is a power of 2")
+    else:
+        print(f"{number} is not a power of 2")
